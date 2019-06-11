73ec6021b6498f61f683c5b5f86c1d5c3abc73c4
MasafumiKabe <msfmkb@gmail.com>
2019-06-11 20:54:03 +0900

    receipts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "receipts#new"
+  resources :receipts, only: [:index, :show, :new, :create, :destroy]
 end
